63e1c26281af2ea689f6c0e75f6db812c0cbbcb8
Kosarevic <kosarevic@gmail.com>
2020-08-06 10:58:25 +0200

    Add project files.

---
+++ DAN_LI_Aleksandar_Kosarevic.sql
@@ -0,0 +1,35 @@
+--If database doesnt exist it is automatically created.
+IF DB_ID('Zadatak_1') IS NULL
+CREATE DATABASE Zadatak_1
+GO
+--Newly created database is set to be in use.
+USE Zadatak_1
+--All tables are reseted clean.
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblSong')
+drop table tblSong
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblUser')
+drop table tblUser
+
+create table tblUser
+(
+UserID int primary key IDENTITY(1,1),
+Username varchar(50),
+Password varchar(50)
+)
+
+create table tblSong
+(
+SongID int primary key IDENTITY(1,1),
+Title varchar(50),
+Author varchar(50),
+Length time,
+UserID int foreign key references tblUser(UserID) not null
+)
+
+insert into tblUser values ('a','a');
+insert into tblSong values ('song 1', 'author 1', '00:01:00', 1);
+insert into tblSong values ('song 2', 'author 2', '00:02:00', 1);
+insert into tblSong values ('song 3', 'author 3', '00:03:00', 1);
+
+select * from tblSong;
+select * from tblUser;
---
+++ Zadatak_1/LoginScreen.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Zadatak_1.Model;
+
+namespace Zadatak_1
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        public static User CurrentUser = new User();
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentUser = new User();
+            SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ToString());
+            try
+            {
+                //User is extracted from the database matching inserted paramaters Username and Password.
+                SqlCommand query = new SqlCommand("SELECT * FROM tblUser WHERE Username=@Username AND Password=@Password", sqlCon);
+                query.CommandType = CommandType.Text;
+                query.Parameters.AddWithValue("@Username", txtUsername.Text);
+                query.Parameters.AddWithValue("@Password", txtPassword.Password);
+                sqlCon.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+
+                CurrentUser = new User();
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    CurrentUser = new User
+                    {
+                        Id = int.Parse(row[0].ToString()),
+                        Username = row[1].ToString(),
+                        Password = row[2].ToString()
+                    };
+                }
+
+                if (CurrentUser != null)
+                {
+                    UserWindow window = new UserWindow(CurrentUser);
+                    window.Show();
+                    Close();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
